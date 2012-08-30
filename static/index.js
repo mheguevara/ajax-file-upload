d872d4f00cfcaa973f17bc0a591973d2eb788204
Muhammet Alay <muhammetalay61@gmail.com>
2012-08-30 22:10:20 +0300

    for heroku

--- static/index.js
+++ static/index.js
@@ -12,9 +12,22 @@ $('form').live('submit', function(){
         },
         error:function(resp){
             $('div#status').html(resp);
+        },
+        xhr:function(){
+            myXhr = $.ajaxSettings.xhr();
+            if(myXhr.upload){
+                myXhr.upload.addEventListener('progress',handleProgress,false);
+            }
+            return myXhr;
         }
     });
     return false;
 });
 
+handleProgress = function(evnt){
+    if(evnt.lengthComputable){
+        var ratio = (evnt.loaded / evnt.total) * 100;
+        $('div#status').html(ratio+"% uploaded");
+    }
+}
 
